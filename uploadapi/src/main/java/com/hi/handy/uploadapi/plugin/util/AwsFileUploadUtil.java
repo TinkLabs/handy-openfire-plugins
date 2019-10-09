1d7d8f4f5e59f52044086cc027f4a65c33df8866
hanjingyang <115877264@qq.com>
2019-10-09 16:28:09 +0800

    make AmazonS3 singleton

--- uploadapi/src/main/java/com/hi/handy/uploadapi/plugin/util/AwsFileUploadUtil.java
+++ uploadapi/src/main/java/com/hi/handy/uploadapi/plugin/util/AwsFileUploadUtil.java
@@ -1,15 +1,13 @@
 package com.hi.handy.uploadapi.plugin.util;
 
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 import java.io.InputStream;
 import org.jivesoftware.util.JiveGlobals;
 import org.slf4j.Logger;
@@ -26,21 +24,23 @@ public class AwsFileUploadUtil {
   private static String DEFAULT_BUCKET_FILE_PATH = "staging";
   private static String FILE_PATH = "https://handy-concierge-chat.s3-ap-southeast-1.amazonaws.com/";
 
-  public static AmazonS3 Init() {
-    AmazonS3 s3 = new AmazonS3Client(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY));
-    s3.setRegion(Region.getRegion(Regions.AP_SOUTHEAST_1));
-    return s3;
+  private static AmazonS3 AMAZON_S3_CLIENT;
+
+  static {
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+    AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials));
+    builder.setRegion(Regions.AP_SOUTHEAST_1.getName());
+    AMAZON_S3_CLIENT = builder.build();
   }
 
   public static String upload(InputStream fileInputStream, String fileName, long fileSize,
       String contentType) {
-    AmazonS3 s3 = Init();
     try {
       ObjectMetadata objectMetadata = new ObjectMetadata();
       objectMetadata.setContentType(contentType);
       objectMetadata.setContentLength(fileSize);
       String key = getKey() + "/" + fileName;
-      s3.putObject(new PutObjectRequest(BUCKET_NAME, key, fileInputStream, objectMetadata));
+      AMAZON_S3_CLIENT.putObject(new PutObjectRequest(BUCKET_NAME, key, fileInputStream, objectMetadata));
       return FILE_PATH + key;
     } catch (Exception e) {
       LOGGER.error("upload error", e);
@@ -57,10 +57,5 @@ public class AwsFileUploadUtil {
       return "production";
     }
     return DEFAULT_BUCKET_FILE_PATH;
-  }
-
-  public static S3Object download(String filePath) {
-    AmazonS3 s3 = Init();
-    return s3.getObject(new GetObjectRequest(BUCKET_NAME, filePath));
   }
 }
